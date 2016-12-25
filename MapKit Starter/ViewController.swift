2b55bd685c5ef65578600a5fff1f1b6af2ecc7c8
Farrukh Askari <farrukh.askari01@gmail.com>
2016-12-25 16:20:59 +0500

    map kit

--- MapKit Starter/ViewController.swift
+++ MapKit Starter/ViewController.swift
@@ -13,4 +13,100 @@ class ViewController: UIViewController {
     @IBOutlet var mapView: MKMapView?
     
     let locationManager = CLLocationManager()
+    let places = Place.getPlaces()
+    
+    func requestLocationAccess() {
+        let status = CLLocationManager.authorizationStatus()
+        
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            return
+        case .denied, .notDetermined:
+            print("location access denied")
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    override func viewDidLoad() {
+        requestLocationAccess()
+//        locationManager.startUpdatingLocation()
+        addAnnotations()
+        addPolyline()
+        addPolygon()
+    }
+    
+    func addAnnotations() {
+        mapView?.delegate = self
+        mapView?.addAnnotations(places)
+        let overlays = places.map { MKCircle(center: $0.coordinate, radius: 100) }
+        mapView?.addOverlays(overlays)
+        
+        var locations = places.map { $0.coordinate }
+        let polyline = MKPolyline(coordinates: &locations, count: locations.count)
+        mapView?.add(polyline)
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+            
+        else {
+            let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "annotationView") ?? MKAnnotationView()
+            annotationView.image = UIImage(named: "place icon")
+            annotationView.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView.canShowCallout = true
+            return annotationView
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        guard let annotation = view.annotation as? Place, let title = annotation.title else { return }
+        
+        let alertController = UIAlertController(title: "Welcome to \(title)", message: "You've selected \(title)", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        if overlay is MKCircle {
+            let renderer = MKCircleRenderer(overlay: overlay)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.blue
+            renderer.lineWidth = 2
+            return renderer
+            
+        } else if overlay is MKPolyline {
+            let renderer = MKPolylineRenderer(overlay: overlay)
+            renderer.strokeColor = UIColor.orange
+            renderer.lineWidth = 3
+            return renderer
+        } else if overlay is MKPolygon {
+            let renderer = MKPolygonRenderer(polygon: overlay as! MKPolygon)
+            renderer.fillColor = UIColor.black.withAlphaComponent(0.5)
+            renderer.strokeColor = UIColor.orange
+            renderer.lineWidth = 2
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
+    func addPolyline() {
+        var locations = places.map { $0.coordinate }
+        let polyline = MKPolyline(coordinates: &locations, count: locations.count)
+        
+        mapView?.add(polyline)
+    }
+    
+    func addPolygon() {
+        var locations = places.map { $0.coordinate }
+        let polygon = MKPolygon(coordinates: &locations, count: locations.count)
+        mapView?.add(polygon)
+    }
 }
